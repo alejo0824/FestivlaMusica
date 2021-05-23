f945349c6941e0a31682c8fad149bf253faeaae5
Alejandro Téllez <alejo082495@gmail.com>
2021-05-22 23:23:54 -0500

    Se crea el archivo gulpfeli.js. Se intala gulp-sass y de define las funciones que por ahora se ejecutaran en el repositorio

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const {src,dest,watch} = require('gulp');
+const sass = require('gulp-sass');
+
+
+const paths ={
+    scss: 'src/scss/base/*.scss'
+}
+
+
+function css (){
+    return src(paths.scss)
+    .pipe(sass({outputStyle:'expanded'}))
+    .pipe(dest('./build/css'))
+}
+
+function convertirArchivos (){
+    watch(paths.scss,css)
+}
+
+exports.convertirArchivos = convertirArchivos;
