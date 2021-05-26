008bfbf4b3b61eba887ce2e6daf7eb51f4cc8d40
Alejandro Téllez <alejo082495@gmail.com>
2021-05-25 21:09:18 -0500

    Se instala gulp-concant que permitira guardar archivos.js almacenados en uno solo llamado bundle.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass');
 const webp = require('gulp-webp');
+const concat = require('gulp-concat');
 
 
 const paths = {
     scss: 'src/scss/**/*.scss',
+    js: 'src/js/**/*.js',
     imagenes: 'src/img/**/*'
 }
 
@@ -15,8 +17,15 @@ function css() {
         .pipe(dest('./build/css'))
 }
 
+function javaScript() {
+    return src(paths.js)
+        .pipe(concat('bundle.js'))
+        .pipe(dest('./build/js'))
+}
+
 function convertirArchivos() {
-    watch(paths.scss, css)
+    watch(paths.scss, css);
+    watch(paths.js, javaScript);
 }
 
 function versionWebp() {
