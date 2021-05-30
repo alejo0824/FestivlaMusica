876ea601fea071e94986a2fe45a1289148a6ef25
Alejandro Téllez <alejo082495@gmail.com>
2021-05-30 16:43:10 -0500

    Se FOptimiza las funciones CSS y JavaScript para que se guarde de manera reducida

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,15 @@ const sass = require('gulp-sass');
 const webp = require('gulp-webp');
 const concat = require('gulp-concat');
 
+//Utilidades CSS
+const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const sourcemaps = require('gulp-sourcemaps');
+
+//Utilidades JS
+const terser = require('gulp-terser-js');
+const rename = require('gulp-rename');
 
 const paths = {
     scss: 'src/scss/**/*.scss',
@@ -12,15 +21,23 @@ const paths = {
 
 
 function css() {
-    return src(paths.scss)
-        .pipe(sass({ outputStyle: 'expanded' }))
-        .pipe(dest('./build/css'))
+    return src(paths.scss) // Se Escoge la ubicación del archvio scss
+        .pipe(sourcemaps.init())
+        .pipe(sass()) // Se pasa de scss a css
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('./build/css')) // se genera una carpeta donde se almacenará el archivo
 }
+
 
 function javaScript() {
     return src(paths.js)
-        .pipe(concat('bundle.js'))
-        .pipe(dest('./build/js'))
+        .pipe(sourcemaps.init())
+        .pipe(concat('bundle.js')) // Se concatena los archivos JS ya que estos no se puende concatenar = que SCSS
+        .pipe(terser())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('./build/js')) //Carpeta Destino 
 }
 
 function convertirArchivos() {
